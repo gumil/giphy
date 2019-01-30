70884bf151cb789c3e7305e72724fb93bf5956a0
Miguel Panelo <miguelrpanelo@gmail.com>
2019-01-30 23:07:40 +0100

    Initialize koin modules

--- app/src/main/java/com/gumil/giphy/App.kt
+++ app/src/main/java/com/gumil/giphy/App.kt
@@ -1,22 +1,33 @@
 package com.gumil.giphy
 
 import android.app.Application
+import com.gumil.giphy.network.createNetworkModule
 import com.squareup.leakcanary.LeakCanary
+import org.koin.core.context.startKoin
 import timber.log.Timber
 
 internal class App : Application() {
 
     override fun onCreate() {
         super.onCreate()
-        if (BuildConfig.DEBUG) {
-            Timber.plant(Timber.DebugTree())
 
-            if (LeakCanary.isInAnalyzerProcess(this)) {
-                // This process is dedicated to LeakCanary for heap analysis.
-                // You should not init your app in this process.
-                return
-            }
-            LeakCanary.install(this)
+        startKoin {
+            modules(createNetworkModule(BuildConfig.DEBUG))
         }
+
+        if (BuildConfig.DEBUG) {
+            initializeDebugTools()
+        }
+    }
+
+    private fun initializeDebugTools() {
+        Timber.plant(Timber.DebugTree())
+
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            // This process is dedicated to LeakCanary for heap analysis.
+            // You should not init your app in this process.
+            return
+        }
+        LeakCanary.install(this)
     }
 }
