91f0bfdfe2c1b09e57fb171080b10042097558c2
Miguel Panelo <miguelrpanelo@gmail.com>
2019-01-30 21:35:54 +0100

    Add model mappers

---
+++ app/src/main/java/com/gumil/giphy/models.kt
@@ -0,0 +1,50 @@
+package com.gumil.giphy
+
+import android.annotation.SuppressLint
+import android.os.Parcelable
+import com.gumil.giphy.network.Giphy
+import com.gumil.giphy.network.Images
+import com.gumil.giphy.network.User
+import kotlinx.android.parcel.Parcelize
+
+@SuppressLint("ParcelCreator")
+@Parcelize
+internal data class GiphyItem(
+        val title: String,
+        val user: UserItem?,
+        val image: ImageItem
+) : Parcelable
+
+@SuppressLint("ParcelCreator")
+@Parcelize
+data class ImageItem(
+        val original: String,
+        val resized: String,
+        val width: Int,
+        val height: Int
+) : Parcelable
+
+@SuppressLint("ParcelCreator")
+@Parcelize
+data class UserItem(
+        val avatarUrl: String,
+        val profileUrl: String,
+        val displayName: String
+) : Parcelable
+
+internal fun Giphy.mapToItem(): GiphyItem {
+    return GiphyItem(title, user?.mapToItem(), images.mapToItem())
+}
+
+internal fun User.mapToItem(): UserItem {
+    return UserItem(avatarUrl, profileUrl, displayName)
+}
+
+internal fun Images.mapToItem(): ImageItem {
+    return ImageItem(
+            downsized.url,
+            fixedWidthDownsampled.url,
+            original.width.toInt(),
+            original.height.toInt()
+    )
+}
