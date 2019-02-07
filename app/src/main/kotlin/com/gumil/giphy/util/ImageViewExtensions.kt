a87328152818b2d804498a72a9036f1852f4c230
Miguel Panelo <miguelrpanelo@gmail.com>
2019-02-07 18:21:01 +0100

    Formatting

--- app/src/main/kotlin/com/gumil/giphy/util/ImageViewExtensions.kt
+++ app/src/main/kotlin/com/gumil/giphy/util/ImageViewExtensions.kt
@@ -7,40 +7,35 @@ import android.view.View
 import android.widget.ImageView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.TransitionOptions
-import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
 import com.bumptech.glide.request.RequestOptions
 
 @SuppressLint("CheckResult")
-internal fun ImageView.load(url: String?,
-                            requestOptions: (RequestOptions.() -> RequestOptions)? = null,
-                            transitionOptions: TransitionOptions<*, Drawable>? = null) {
-    url?.let {
+internal fun ImageView.load(
+    url: String?,
+    requestOptions: (RequestOptions.() -> RequestOptions)? = null,
+    transitionOptions: TransitionOptions<*, Drawable>? = null
+) {
+    url?.let { imageUrl ->
         visibility = View.VISIBLE
         Glide.with(context)
-                .load(Uri.parse(url))
-                .apply {
-                    requestOptions?.invoke(RequestOptions())?.let {
-                        apply(it)
-                    }
-
-                    transitionOptions?.let { transition(it) }
+            .load(Uri.parse(imageUrl))
+            .apply {
+                requestOptions?.invoke(RequestOptions())?.let {
+                    apply(it)
                 }
-                .into(this)
+
+                transitionOptions?.let { transition(it) }
+            }
+            .into(this)
     } ?: let { visibility = View.GONE }
 }
 
 @SuppressLint("CheckResult")
-internal fun ImageView.load(url: String?,
-                            requestOptions: (RequestOptions.() -> RequestOptions)? = null) {
+internal fun ImageView.load(
+    url: String?,
+    requestOptions: (RequestOptions.() -> RequestOptions)? = null
+) {
     load(url, requestOptions, null)
-}
-
-@SuppressLint("CheckResult")
-internal fun ImageView.load(drawable: Drawable) {
-    Glide.with(context)
-            .load(drawable)
-            .transition(DrawableTransitionOptions.withCrossFade())
-            .into(this)
 }
 
 internal fun ImageView.setHeight(height: Int) {
