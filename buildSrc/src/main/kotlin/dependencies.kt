234bb724ae7ce48d16f99ee72a047d370265789e
Miguel Panelo <miguelrpanelo@gmail.com>
2020-06-20 11:24:15 +0200

    Update project after a long time

--- buildSrc/src/main/kotlin/dependencies.kt
+++ buildSrc/src/main/kotlin/dependencies.kt
@@ -1,26 +1,26 @@
 @file:Suppress("unused", "ClassName")
 
 object versions {
-    const val kotlin = "1.3.41"
-    const val detekt = "1.0.0-RC16"
-    const val moshi = "1.8.0"
-    const val retrofit = "2.6.0"
-    const val okhttp = "4.0.1"
-    const val koin = "2.0.1"
-    const val kaskade = "0.3.6"
+    const val kotlin = "1.3.72"
+    const val detekt = "1.10.0-RC1"
+    const val moshi = "1.9.3"
+    const val retrofit = "2.9.0"
+    const val okhttp = "4.7.2"
+    const val koin = "2.1.6"
+    const val kaskade = "0.3.8"
     const val livedata = "2.0.0"
-    const val navigation = "2.1.0-beta02"
-    const val coroutines = "1.3.0-RC2"
+    const val navigation = "2.2.2"
+    const val coroutines = "1.3.7"
 }
 
 object build {
-    const val androidGradle = "com.android.tools.build:gradle:3.4.2"
+    const val androidGradle = "com.android.tools.build:gradle:4.0.0"
     const val kotlinGradle = "org.jetbrains.kotlin:kotlin-gradle-plugin:${versions.kotlin}"
 
     object android {
-        const val buildToolsVersion = "29.0.1"
+        const val buildToolsVersion = "30.0.0"
         const val compileSdkVersion = 29
-        const val minSdkVersion = 21
+        const val minSdkVersion = 23
         const val targetSdkVersion = 29
     }
 }
@@ -31,15 +31,15 @@ object projectPlugins {
 
 object libs {
     object android {
-        const val appcompat = "androidx.appcompat:appcompat:1.1.0-rc01"
-        const val ktx = "androidx.core:core-ktx:1.2.0-alpha01"
-        const val recyclerView = "androidx.recyclerview:recyclerview:1.1.0-beta01"
+        const val appcompat = "androidx.appcompat:appcompat:1.1.0"
+        const val ktx = "androidx.core:core-ktx:1.3.0"
+        const val recyclerView = "androidx.recyclerview:recyclerview:1.1.0"
 
         object navigation {
             const val fragment = "androidx.navigation:navigation-fragment-ktx:${versions.navigation}"
             const val ui = "androidx.navigation:navigation-ui-ktx:${versions.navigation}"
         }
-        const val material = "com.google.android.material:material:1.1.0-alpha08"
+        const val material = "com.google.android.material:material:1.3.0-alpha01"
     }
 
     object kotlin {
@@ -68,7 +68,7 @@ object libs {
     }
 
     const val timber = "com.jakewharton.timber:timber:4.7.1"
-    const val leakCanary = "com.squareup.leakcanary:leakcanary-android:1.6.3"
+    const val leakCanary = "com.squareup.leakcanary:leakcanary-android:2.4"
 
     object kaskade {
         const val core = "dev.gumil.kaskade:core:${versions.kaskade}"
@@ -76,7 +76,7 @@ object libs {
         const val livedata = "dev.gumil.kaskade:livedata:${versions.kaskade}"
     }
 
-    const val glide = "com.github.bumptech.glide:glide:4.9.0"
+    const val glide = "com.github.bumptech.glide:glide:4.11.0"
 
     object test {
         const val junit = "junit:junit:4.12"
@@ -86,8 +86,8 @@ object libs {
 
         const val livedata = "androidx.arch.core:core-testing:${versions.livedata}"
 
-        const val espresso = "androidx.test.espresso:espresso-core:3.1.0"
-        const val androidTestRunner = "androidx.test:runner:1.1.0"
+        const val espresso = "androidx.test.espresso:espresso-core:3.2.0"
+        const val androidTestRunner = "androidx.test:runner:1.2.0"
 
         const val coroutines = "org.jetbrains.kotlinx:kotlinx-coroutines-test:${versions.coroutines}"
     }
